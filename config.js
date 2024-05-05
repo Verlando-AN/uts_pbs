7acdf9411540a89b67b40441d17cc1c010b9ca0a
Verlando-AN <121385167+Verlando-AN@users.noreply.github.com>
2024-05-06 01:50:29 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,10 +3,10 @@ const mysql = require('mysql');
 
 // Variabel koneksi database
 const db = mysql.createConnection({
-  host: 'sql6.freesqldatabase.com', // Host database
-  user: 'sql6702751', // Pengguna database
-  password: 'ulTfR6Lyi9', // Kata sandi database
-  database: 'sql6702751' // Mengasumsikan 'mahasiswa' adalah nama database Anda
+  host: 'localhost', // Host database
+  user: 'root', // Pengguna database
+  password: '', // Kata sandi database
+  database: 'mahasiswa' // Mengasumsikan 'mahasiswa' adalah nama database Anda
 });
 
 // Mengekspor variabel koneksi database untuk digunakan di modul lain
