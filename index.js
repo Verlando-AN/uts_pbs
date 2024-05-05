fa75e33e70499d14c50551c7e0eb8af22c19e215
Verlando-AN <121385167+Verlando-AN@users.noreply.github.com>
2024-05-06 01:53:57 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3002;
+const port = 3002;
 
 const bodyParser = require('body-parser');
 const db = require('./connection.js');
