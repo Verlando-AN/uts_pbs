c347b6215517bde47f2865af4a1a5fe55f9b42fe
Verlando-AN <121385167+Verlando-AN@users.noreply.github.com>
2024-05-06 01:20:10 +0700

    Update index.js

--- index.js
+++ index.js
@@ -9,39 +9,79 @@ const response = require('./response.js')
 // Middleware for parsing JSON bodies
 app.use(bodyParser.json());
 
-app.get('/mahasiswa', (req,res)=>{
-    const sql ='SELECT * FROM tb_mahasiswa'
-    db.query(sql,(error, result)=>{
-        response(200,result,'data mahasiswa', res)
-    })
-})
+app.get("/", (req, res) => {
+  response(200, "welcome to api", "Selamat datang di api service", res);
+});
 
-app.get('/mahasiswa/:npm', (req, res)=>{
-  const npm = req.params.npm
-  const sql = `SELECT * FROM tb_mahasiswa where npm='${npm}'`
-  db.query(sql, (err, result)=>{
-    if(err) throw err
-    response(200,result,"get detail mahasiswa",res)
-  })
-})
+app.get("/mahasiswa", (req, res) => {
+  const sql = "SELECT * FROM tb_mahasiswa";
+  db.query(sql, (err, result) => {
+    if (err) response(500, "error", err, res);
+    response(200, result, "get all data mahasiswa", res);
+  });
+});
 
-app.post('/mahasiswa', (req, res) => {
-  const {nama, npm, alamat }=req.body
-  const sql = `INSERT INTO tb_mahasiswa (nama, npm , alamat) values ('${nama}','${npm}','${alamat}');`
+app.get("/mahasiswa/:npm", (req, res) => {
+  const url_data = req.params.npm;
+  const sql = `SELECT * FROM tb_mahasiswa WHERE npm_mhs='${url_data}'`;
+  db.query(sql, (err, result) => {
+    if (err) response(500, "error", err, res);
+    response(200, result, "get data mahasiswa by npm", res);
+  });
+});
 
-  db.query(sql,(error, fields)=>{
-    if(error) response(500, 'invalid', `${nama} degan npm ${npm} sudah di tambahkan`, res)
-    if(fields?.affectedRows){
-      const data ={
-        isSucces: fields.affectedRows,
-        id:fields.insertId,
-      }
-      response(200,data,"data berhasil di simpan",res)
+app.post("/mahasiswa", (req, res) => {
+  const { nama, npm, alamat } = req.body;
+  const sql = `INSERT INTO tb_mahasiswa (nama_mhs, npm_mhs, alamat_mhs) VALUES ('${nama}', '${npm}', '${alamat}')`;
+
+  db.query(sql, (err, fields) => {
+    if (err) response(500, "error", err, res);
+    if (fields?.affectedRows) {
+      const data = {
+        isSuccess: fields.affectedRows,
+        id: fields.insertId,
+      };
+      response(200, data, "Data Berhasil Ditambahkan", res);
     }
-    
-  })
-})
+  });
+});
+
+app.put("/mahasiswa/:npm", (req, res) => {
+  const { nama, alamat } = req.body;
+  const npm = req.params.npm;
+  const sql = `UPDATE tb_mahasiswa SET nama_mhs='${nama}', alamat_mhs='${alamat}' WHERE npm_mhs='${npm}'`;
+
+  db.query(sql, (err, fields) => {
+    if (err) response(500, "error", err, res);
+    if (fields?.affectedRows) {
+      const data = {
+        isSuccess: fields.affectedRows,
+        message: "Data Berhasil Diupdate",
+      };
+      response(200, data, "Data Berhasil Diupdate", res);
+    } else {
+      response(404, "not found", "Data tidak ditemukan", res);
+    }
+  });
+});
+
+app.delete("/mahasiswa/:npm", (req, res) => {
+  const npm = req.params.npm;
+  const sql = `DELETE FROM tb_mahasiswa WHERE npm_mhs ='${npm}'`;
+  db.query(sql, (err, fields) => {
+    if (err) response(500, "error", err, res);
+    if (fields?.affectedRows) {
+      const data = {
+        isSuccess: fields.affectedRows,
+        message: "Data Berhasil Dihapus",
+      };
+      response(200, data, "Data Berhasil Dihapus", res);
+    } else {
+      response(404, "not found", "Data tidak ditemukan", res);
+    }
+  });
+});
 
 app.listen(port, () => {
-  console.log('Running in port ${port}');
+  console.log(`Running on port ${port}`);
 });
